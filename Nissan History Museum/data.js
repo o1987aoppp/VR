950c135c9969e4cb5cb2c91c4baf863243011a3a
o1987aoppp <jas59487a@gmail.com>
2024-04-11 11:37:41 +0800

    upload

---
+++ Nissan History Museum/data.js
@@ -0,0 +1,272 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-nissan-history-inside",
+      "name": "nissan history inside",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -1.5494877300119256,
+        "pitch": 0.10033693382914599,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.544781741008535,
+          "pitch": 0.03906414220661425,
+          "rotation": 0,
+          "target": "3-nissan-history-inside-vq"
+        },
+        {
+          "yaw": -1.7145291884780427,
+          "pitch": 0.13778706484790249,
+          "rotation": 5.497787143782138,
+          "target": "4-nissan-history-inside-2f"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.973818908551806,
+          "pitch": 0.09196954496568566,
+          "title": "第一代 Leaf",
+          "text": "第一代 Leaf 於2009年推出，為世界第一款量產純電動車，使用鋰電池與80kw交流電馬達驅動"
+        }
+      ]
+    },
+    {
+      "id": "1-nissan-history-inside-sr",
+      "name": "nissan history inside SR",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -1.63195465911123,
+        "pitch": 0.6083181349579654,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8994699479080879,
+          "pitch": 0.1896347356237449,
+          "rotation": 0,
+          "target": "0-nissan-history-inside"
+        },
+        {
+          "yaw": 2.9606424837803917,
+          "pitch": 0.8054784682574159,
+          "rotation": 0,
+          "target": "2-nissan-history-inside-rb"
+        },
+        {
+          "yaw": -1.117578179267193,
+          "pitch": 0.4382806056881208,
+          "rotation": 5.497787143782138,
+          "target": "3-nissan-history-inside-vq"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6349486068923476,
+          "pitch": 0.330002652351844,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-nissan-history-inside-rb",
+      "name": "nissan history inside RB",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -1.4353667526722766,
+        "pitch": 0.5115281019337594,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06128283662601319,
+          "pitch": 0.7791319110828443,
+          "rotation": 0,
+          "target": "1-nissan-history-inside-sr"
+        },
+        {
+          "yaw": -2.6278381802732955,
+          "pitch": 0.08261816509022779,
+          "rotation": 6.283185307179586,
+          "target": "4-nissan-history-inside-2f"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5203137577738204,
+          "pitch": 0.3156051071011099,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-nissan-history-inside-vq",
+      "name": "nissan history inside VQ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 1.5558840610506985,
+        "pitch": 0.5290351378035432,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.411855049704789,
+          "pitch": 0.35634154900627024,
+          "rotation": 5.497787143782138,
+          "target": "0-nissan-history-inside"
+        },
+        {
+          "yaw": 3.046462002924142,
+          "pitch": 0.3428517711655896,
+          "rotation": 0,
+          "target": "1-nissan-history-inside-sr"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.9148204252397623,
+          "pitch": 0.25343785326153245,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-nissan-history-inside-2f",
+      "name": "nissan history inside 2F",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -1.6366914107987718,
+        "pitch": 0.2292147871622774,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.955599670916051,
+          "pitch": 0.5509423197675538,
+          "rotation": 0.7853981633974483,
+          "target": "2-nissan-history-inside-rb"
+        },
+        {
+          "yaw": -1.1671121503101123,
+          "pitch": 0.15416288192628258,
+          "rotation": 0.7853981633974483,
+          "target": "0-nissan-history-inside"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.8286315819124894,
+          "pitch": -0.01582678072157684,
+          "title": "Nissan 橫濱工廠",
+          "text": "<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Nissan History Museum",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
