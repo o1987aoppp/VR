3e225bdacc8540b2833137671c0fe4f4af01bce0
o1987aoppp <jas59487a@gmail.com>
2024-04-11 12:00:23 +0800

    rename

---
+++ Nissan Gallery/data.js
@@ -0,0 +1,373 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-nissan-gallery-",
+      "name": "Nissan Gallery ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5676221953704088,
+        "pitch": 0.010177044031426163,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0266321662668005,
+          "pitch": 0.16942274002291313,
+          "rotation": 5.497787143782138,
+          "target": "1-r32-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.49582223918501,
+          "pitch": -0.34871754048979753,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-r32-",
+      "name": "R32 ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "yaw": 0.9937691047069706,
+        "pitch": 0.19590553811657685,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.23302919094232522,
+          "pitch": 0.19336043407629333,
+          "rotation": 0.7853981633974483,
+          "target": "0-nissan-gallery-"
+        },
+        {
+          "yaw": -0.8885201387644486,
+          "pitch": 0.2686945472278044,
+          "rotation": 0,
+          "target": "2-r35-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.2047631580917706,
+          "pitch": -0.03150653809333903,
+          "title": "R32 GT-R",
+          "text": "第三代Skyline GT-R(代號R32)，使用RB26DETT雙渦輪增壓引擎與五速手動變速箱，並配備Attesa ETS四輪驅動系統。"
+        },
+        {
+          "yaw": 2.452156129391719,
+          "pitch": 0.4268266286010878,
+          "title": "350Z Roadster",
+          "text": "第五代 Fairlady Z (代號Z33)，使用VQ35DE V6自然進氣引擎搭配五速手動變速箱，於2004年推出Roadster版本"
+        },
+        {
+          "yaw": -0.23495886651439157,
+          "pitch": 0.25207057807594424,
+          "title": "Fairlady Z",
+          "text": "第一代 Fairlady Z (代號S30/Z29)，使用L20A 2.0升直列六缸引擎搭配五速手動變速箱。<br>外銷至美國的版本名為Datsun 240Z，使用L24A 2.4升值列六缸引擎。"
+        },
+        {
+          "yaw": -0.2705056082645587,
+          "pitch": 0.6318576656682833,
+          "title": "Z32",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "2-r35-",
+      "name": "R35 ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1502,
+      "initialViewParameters": {
+        "yaw": -2.2991261168667805,
+        "pitch": 0.17280601375954063,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.22353088796216092,
+          "pitch": 0.16337710198573596,
+          "rotation": 11.780972450961727,
+          "target": "6--skyline"
+        },
+        {
+          "yaw": 0.25119575024778307,
+          "pitch": 0.1775008093227246,
+          "rotation": 0,
+          "target": "5-r35-gt3"
+        },
+        {
+          "yaw": -2.86078814555043,
+          "pitch": 0.26622952496556174,
+          "rotation": 0.7853981633974483,
+          "target": "3-r35-"
+        },
+        {
+          "yaw": -2.3655678770911024,
+          "pitch": -0.16774337939890138,
+          "rotation": 0,
+          "target": "1-r32-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.0278515739302385,
+          "pitch": 0.061446738587905614,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "3-r35-",
+      "name": "R35 車外",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1505.5,
+      "initialViewParameters": {
+        "yaw": 0.2760374309226066,
+        "pitch": 0.30022894208823914,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2918052159490472,
+          "pitch": 0.4574017808198487,
+          "rotation": 0,
+          "target": "4-r35-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.9971394689719109,
+          "pitch": -0.013182761278066835,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "4-r35-",
+      "name": "R35 車內",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1506.5,
+      "initialViewParameters": {
+        "yaw": 1.5531017445166144,
+        "pitch": 0.22988629300013486,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5822685351284402,
+          "pitch": 0.24098565027731667,
+          "rotation": 7.853981633974483,
+          "target": "3-r35-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-r35-gt3",
+      "name": "R35 GT3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500.5,
+      "initialViewParameters": {
+        "yaw": -0.08904906751866193,
+        "pitch": 0.19845235861275157,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7489555989478944,
+          "pitch": 0.1328701888583126,
+          "rotation": 0,
+          "target": "6--skyline"
+        },
+        {
+          "yaw": 2.149845660132553,
+          "pitch": 0.14221289380420998,
+          "rotation": 0,
+          "target": "2-r35-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.19396463340994963,
+          "pitch": 0.03365933299924251,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6--skyline",
+      "name": "新 Skyline",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1510,
+      "initialViewParameters": {
+        "yaw": 1.0685696252065995,
+        "pitch": 0.14247861643992543,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0196073895000453,
+          "pitch": 0.15117306349134196,
+          "rotation": 5.497787143782138,
+          "target": "5-r35-gt3"
+        },
+        {
+          "yaw": -1.852445832621374,
+          "pitch": 0.16607705513939308,
+          "rotation": 0,
+          "target": "2-r35-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8357736745257629,
+          "pitch": -0.05277949806913185,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Nissan Gallery",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
